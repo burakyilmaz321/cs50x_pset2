de6d8b3dbdaeed7faa8f4c32ca586068152734e8
burakyilmaz321 <burakyilmaz321@gmail.com>
2015-06-30 19:20:08 +0300

    Update initialize.c

--- initialize.c
+++ initialize.c
@@ -5,20 +5,20 @@
 
 int main(void)
 {
-   # Get user input
+   /* Get user input */
    char name[] = GetString();
    char *token;
    
-   # Split the string at first space
+   /* Split the string at first space */
    token = strtok(name, " ");
    
-   # Keep spliting until there is nothing to split
+   /* Keep spliting until there is nothing to split */
    while(token != NULL) 
    {
-      # Print the first part splitted
+      /* Print the first part splitted */
       printf("%c", toupper(token[0]));
       
-      # Split the remaining
+      /* Split the remaining */
       token = strtok(NULL, " ");
    }
    printf("\n");
