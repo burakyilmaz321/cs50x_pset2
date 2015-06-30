07d43beaa45f71484f8ef11daa3e2a4a0502707f
burakyilmaz321 <burakyilmaz321@gmail.com>
2015-06-30 19:50:04 +0300

    Add a function called initialize

--- initialize.c
+++ initialize.c
@@ -3,19 +3,29 @@
 #include <ctype.h>
 #include <cs50.h>
 
+void initialize(char str[]);
+
 int main(void)
 {
    /* Get user input */
    char name[] = GetString();
+   
+   /* Call the initializing function with the name user gave */
+   initialize(name);
+}
+
+/* Define the initializing function */
+void initialize(char str[])
+{
    char *token;
    
-   /* Split the string at first space */
-   token = strtok(name, " ");
+   /* Split the string at the first space */
+   token = strtok(str, " ");
    
    /* Keep spliting until there is nothing to split */
    while(token != NULL) 
    {
-      /* Print the first part splitted */
+      /* Print first letter of splitted part */
       printf("%c", toupper(token[0]));
       
       /* Split the remaining */
