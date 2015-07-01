571c8b5fde571049ff4a4903d631909274ff6c0f
burakyilmaz321 <burakyilmaz321@gmail.com>
2015-07-01 16:21:40 -0400

    Update ceasar.c

--- ceasar.c
+++ ceasar.c
@@ -3,18 +3,41 @@
 #include <string.h>
 #include <ctype.h>
 
+/* Caesar Chiper */
 int main(int argc, string argv[])
 {
-  if(argc == 2)
-  {
-    int k = atoi(arg [1]);
-    char** str = GetString();
-    
-    return 0;
-  }
-  else
-  {
-    printf("You must enter one argument.");
-    return 1;
-  }
+    /* Check if there is exactly 2 arguments */
+    if (argc == 2)
+    {
+        /* Key */
+        int k = atoi(argv[1]);
+        
+        /* Get user input */
+        char * str = GetString();
+        for(int i = 0, n = strlen(str); i < n; i++)
+        {
+            /* Crypt uppercase characters */
+            if (isalpha(str[i]) && isupper(str[i]))
+            {   
+                printf("%c", ((str[i] - 65 + k) % 26) + 65);
+            }
+                /* Crypt lowercase characters */
+            else if(isalpha(str[i]) && islower(str[i]))
+            {   
+                printf("%c", ((str[i] - 97 + k) % 26) + 97); 
+            }   
+                /* Print non-alpha characters without crypting */
+            else
+            {
+                printf("%c", str[i]);
+            }
+        }
+        printf("\n");
+        return 0;
+    }
+    else
+    {
+        printf("You must enter one argument.\n");
+        return 1;
+    }
 }
